9853ddc37bf0dcbcb64ba8eaf63a2c833ce6530e
Adithya Aiyaswami <aiyasa@amazon.com>
2023-03-13 07:34:55 +0530

    Adding a basic spring boot application.

---
+++ src/main/java/com/adimessi30/elevatorsystem/ElevatorSystem.java
@@ -0,0 +1,12 @@
+package com.adimessi30.elevatorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevatorSystem.class);
+    }
+}
